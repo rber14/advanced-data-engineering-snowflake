a9370519f0e1e2c28862d38b53f2cb33ebcaeeb8
Roberto <roberto.student@outlook.com>
2025-12-06 20:42:36 -0800

    Revert "Staging"

--- module-1/hamburg_weather/pipeline/data/load_tasty_bytes.sql
+++ module-1/hamburg_weather/pipeline/data/load_tasty_bytes.sql
@@ -57,16 +57,14 @@ raw zone table build
 -- country table build
 
 -- todo: complete table build
-CREATE OR ALTER TABLE {{env}}_tasty_bytes.raw_pos.country
+CREATE TABLE {{env}}_tasty_bytes.raw_pos.country
 (
    country_id NUMBER(18,0),
    country VARCHAR(16777216),
    iso_currency VARCHAR(3),
    iso_country VARCHAR(2),
    city VARCHAR(16777216),
-   city_population VARCHAR(16777216),
-   city_id NUMBER(19,0)
-   
+   city_population VARCHAR(16777216)
 );
 
 
@@ -294,8 +292,18 @@ raw zone table load
 USE WAREHOUSE demo_build_wh;
 
 
-COPY INTO {{env}}_tasty_bytes.raw_pos.country
-FROM @{{env}}_tasty_bytes.public.s3load/raw_pos/country/;
+-- country table load
+-- COPY INTO {{env}}_tasty_bytes.raw_pos.country
+-- (
+--    country_id,
+--    country,
+--    iso_currency,
+--    iso_country,
+--    city_id,
+--    city,
+--    city_population
+-- )
+-- FROM @{{env}}_tasty_bytes.public.s3load/raw_pos/country/;
 
 
 -- franchise table load
